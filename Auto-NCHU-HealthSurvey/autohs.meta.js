4e46b3ca8cb53ead69615a4dc66fb19c7036f1ca
王證傑 <new.computeruser.old@gmail.com>
2022-03-26 02:53:54 +0800

    v0.1.2

--- Auto-NCHU-HealthSurvey/autohs.meta.js
+++ Auto-NCHU-HealthSurvey/autohs.meta.js
@@ -1,14 +1,14 @@
 // ==UserScript==
 // @name         NCHU Health Form Filler
 // @namespace    https://avenger2.nchu.edu.tw/HealthSurvey/
-// @version      0.1.1
+// @version      0.1.2
 // @date         2022-03-25
 // @description  自動幫你填寫疫苗表單
 // @homepage     https://www.nchu.edu.tw/
 // @author       Axisflow
 // @match        https://avenger2.nchu.edu.tw/*
 // @icon         https://www.nchu.edu.tw/favicon.ico
-// @updateURL    https://axialflow.github.io/GitCloud-Public/Auto-NCHU-HealthSurvey/meta-update.js
-// @downloadURL  https://axialflow.github.io/GitCloud-Public/Auto-NCHU-HealthSurvey/main.js
+// @updateURL    https://axialflow.github.io/GitCloud-Public/Auto-NCHU-HealthSurvey/autohs.meta.js
+// @downloadURL  https://axialflow.github.io/GitCloud-Public/Auto-NCHU-HealthSurvey/autohs.user.js
 // @grant        none
 // ==/UserScript==
