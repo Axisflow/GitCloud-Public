6e8f81ed95617cbbc3987d051ee30b26ff4ccb3e
王證傑 <new.computeruser.old@gmail.com>
2023-06-19 22:02:37 +0800

    Add files via upload

---
+++ NCHU-PortalLogin/pl.meta.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         NCHU Portal Helper
+// @namespace    https://idp.nchu.edu.tw/
+// @version      0.1.6
+// @description  興大入口自動登入
+// @homepage     https://www.nchu.edu.tw/
+// @author       Axisflow
+// @match        https://idp.nchu.edu.tw/*
+// @icon         https://www.nchu.edu.tw/favicon.ico
+// @updateURL    https://axisflow.github.io/GitCloud-Public/NCHU-PortalLogin/pl.meta.js
+// @downloadURL  https://axisflow.github.io/GitCloud-Public/NCHU-PortalLogin/pl.user.js
+// @grant        unsafeWindow
+// @grant        GM_setValue
+// @grant        GM_getValue
+// @grant        window.close
+// ==/UserScript==
+
