8251a38c74a6ce77a4a6756901314922bf601b80
王證傑 <new.computeruser.old@gmail.com>
2023-09-03 15:43:00 +0800

    Update pl.meta.js

--- NCHU-PortalLogin/pl.meta.js
+++ NCHU-PortalLogin/pl.meta.js
@@ -1,17 +1,16 @@
 // ==UserScript==
-// @name         NCHU Portal Helper
+// @name         NCHU Portal Helper for Loginning
 // @namespace    https://idp.nchu.edu.tw/
-// @version      0.1.6
+// @version      0.1.7
 // @description  興大入口自動登入
 // @homepage     https://www.nchu.edu.tw/
 // @author       Axisflow
 // @match        https://idp.nchu.edu.tw/*
 // @icon         https://www.nchu.edu.tw/favicon.ico
-// @updateURL    https://axisflow.github.io/GitCloud-Public/NCHU-PortalLogin/pl.meta.js
-// @downloadURL  https://axisflow.github.io/GitCloud-Public/NCHU-PortalLogin/pl.user.js
+// @updateURL    https://axisflow.github.io/NCHU-Website-Helper/Portal-Login/pl.meta.js
+// @downloadURL  https://axisflow.github.io/NCHU-Website-Helper/Portal-Login/pl.user.js
 // @grant        unsafeWindow
 // @grant        GM_setValue
 // @grant        GM_getValue
 // @grant        window.close
 // ==/UserScript==
-
